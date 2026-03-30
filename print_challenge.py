a8450a277c6891c004cf88272aab560458e94d09
Alejandro Diaz <diaz360arteaga@gmail.com>
2026-03-30 13:31:43 -0600

    Final Code

---
+++ print_challenge.py
@@ -0,0 +1,14 @@
+#Use PRINT() to recrrate this exact output 
+#Your are allowed to use only one PRINT()
+#OUTPUT: 
+#Your Learning Path:
+#    -Python Basics
+#    -Data Engineering
+#    -AI
+print("Your Learning Path:\n\t- Python Basics \n\t- Data Engineering \n\t- AI")
+
+#OR
+print("""Your Learning Path:
+    \t- Python Basics 
+    \t- Data Engineering 
+    \t- AI""")
