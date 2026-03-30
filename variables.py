fdea8a32be2e68326c7d95cc872852f3d51de01f
Alejandro Diaz <diaz360arteaga@gmail.com>
2026-03-30 13:57:28 -0600

    Final code

---
+++ variables.py
@@ -0,0 +1,15 @@
+print("My name is Alejandro")
+print("Alejandro is learning Python")
+print("Alejandro wants to become Pyhon expert")
+
+name = "Alejandro Diaz"
+
+print("My name is", name)
+print(name, "is learning Python")
+print(name, "wants to become Pyhon expert")
+
+name = "Diaz"
+language = "Python" 
+print("My name is", name)
+print(name, "is learning", language)
+print(name, "wants to become", language, "expert")
