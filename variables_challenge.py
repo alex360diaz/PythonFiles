661846fbddacb8db52dc25ff23da6e09562a448d
Alejandro Diaz <diaz360arteaga@gmail.com>
2026-03-31 09:19:45 -0600

    Final code

---
+++ variables_challenge.py
@@ -0,0 +1,12 @@
+#Print the following three lines
+#   info@datawithbaraa.com
+#   support@datawithbaraa.com
+#   www.datawithbaraa.com
+#Add a variable to make it dynamic
+
+domain = "datawithbaraa.com"
+print("info@" + domain +
+      "\nsupport@" + domain +
+      "\nwww." + domain)
+userEmail = input("Write your email: ")
+print("Your mail is :\n",userEmail)
