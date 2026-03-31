5c892308043ae92a675f0b9280a1fe876bdf8845
Alejandro Diaz <diaz360arteaga@gmail.com>
2026-03-31 13:43:59 -0600

    Final code

---
+++ 2-Working_with_strings/math_function.py
@@ -0,0 +1,19 @@
+# Math
+#   len(value) --> returns the number of items in a value
+#                  returns tje number of characters in a string
+password = "123dsadfasda"
+print(len(password))
+
+if len(password) < 8:
+    print("Your Password is too short")
+else:
+    print("Your Password it's fine")
+
+# Math 2.0
+#   count(substring) --> returns how often a word appears in the string
+text = """
+Python is easy learn.
+Python is powerful.
+Many people love python.
+"""
+print(text.count("Python"))
