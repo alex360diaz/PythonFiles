e34f2316d04e30b84e7181d06f7da8101f1e1505
Alejandro Diaz <diaz360arteaga@gmail.com>
2026-03-31 09:45:02 -0600

    Final Code

---
+++ data_types.py
@@ -0,0 +1,23 @@
+#Data Types
+a = 10      #int
+b = 3.15    #float
+c = "Hello" #str
+d = '123'   #str
+e = True    #bool
+f = False   #bool
+g = None    #None
+h = ""      #str - blank
+i = " "     #str - Empty Space
+
+print("Int   --> a: ",a,
+      "\nFloat --> b: ",b,
+      "\nString -> c: ",c,
+      "\nString -> d: ",d,
+      "\nBool  --> e: ",e,
+      "\nBool  --> f: ",f,
+      "\nNone  --> g: ",g,
+      "\nStr-Bl -> h: ",h,
+      "\nStr-Sp -> i: ",i,     
+      
+      
+      )
