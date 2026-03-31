d194ca9ffd9c62552a8a9474b4b24f810d058b48
Alejandro Diaz <diaz360arteaga@gmail.com>
2026-03-31 15:53:35 -0600

    Final code

---
+++ 2-Working_with_strings/transformations.py
@@ -0,0 +1,32 @@
+# Transformations
+#   replace(old, new) : swaps part of the text with something new
+#Example 1 : We we need to change de , for . becouse europe use , for decimals and other parts use . for decimals
+price = "33,55"
+print("Example 1")
+print(price.replace(",", "."))
+
+#Example 2 When user type their phone number like 0155/3234/56 intead of -
+phone = "101/3455/67"
+print("\nExample 2")
+print(phone.replace("/","-"))
+#Also intead of insrting something new we can removed character for a clean number
+print(phone.replace("/",""))
+
+# Example 3 to convert a string price to a float price
+cost = "$1,299.99"
+print("\nExample 3")
+print(cost.replace("$","").replace(",",""))
+
+#Example 4 
+#Replace the two first occurrence of the word "one":
+txt = "one one was a race horse, two two was one too."
+x = txt.replace("one", "three", 2)
+print("\nExample 4")
+print(x)
+
+#Challenge
+#Convert the messy phone number into a clean number format with only digits
+#           "+49 (176) 123-4567"
+messy_number = "+49 (176) 123-4567"
+print("\nChallenge")
+print(messy_number.replace("+","").replace("(","").replace(")","").replace("-","").replace(" ",""))
