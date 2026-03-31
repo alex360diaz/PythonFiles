1a8cf3eee61d1047c0afbf82dba851e36ddbce1d
Alejandro Diaz <diaz360arteaga@gmail.com>
2026-03-31 13:11:05 -0600

    Final code

---
+++ 2-Working_with_strings/type_function.py
@@ -0,0 +1,11 @@
+# Types
+name = "Alejandro"
+print("Name:",name,"\nType:",type(name))
+
+age = 27
+print("Age:",age,"\nType:",type(age))
+
+# To concatenate 2 values 1 from str and 1 from int
+# We need str()
+
+print("Your age is: " + str(age))
