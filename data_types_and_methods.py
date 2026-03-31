53af6782b0c2c008ca7608c1f08017582fc9fef1
Alejandro Diaz <diaz360arteaga@gmail.com>
2026-03-31 11:19:41 -0600

    Final Code

---
+++ data_types_and_methods.py
@@ -0,0 +1,19 @@
+text = 'hi'
+num = 10
+
+print(text)
+print(num)
+# type() --> returns the data type of a value,
+#            so you know what kind of object it is.
+print(type(text))
+print(type(num))
+
+# len() --> gives the total count of items inside a vulue 
+#           helping you measure it's length
+print(len(text))
+
+# upper() --> Converts all letters in a string to uppercase
+print(text.upper())
+
+# bit_lenght() --> returns the lenght of a number in binary.
+print(num.bit_length())
