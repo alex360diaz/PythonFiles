615d51451aa2146740c7835f5f2820500b6e6ea1
Alejandro Diaz <diaz360arteaga@gmail.com>
2026-03-31 19:41:17 -0600

    Finale code for f-string.py

---
+++ 2-Working_with_strings/f-string.py
@@ -0,0 +1,17 @@
+# f-string
+# modern, super-easy way to format and bouid strings
+# "f" stands for "formatted"
+# lets you easily put varibales and expressions
+# directly inside string value
+name = "Alex"
+age = 27
+is_student = False
+
+print(
+    f"My name is {name}, I am {age} years old, and my student status is {is_student}.")
+
+# We can use f-string to use math
+print(f"5 x 5 = {5 * 5}")
+
+# To use brakets and get printed is like this:
+print(f"{{This is how to use it.}}")
