9406c0c0bd6d0be381f23b07298b4fc5ecdb85b0
Alejandro Diaz <diaz360arteaga@gmail.com>
2026-04-01 20:22:39 -0600

    Final code for repeating operator

---
+++ 2-Working_with_strings/repeating.py
@@ -0,0 +1,12 @@
+# Repeating
+# 'string' * number --> repeats the string multiple times
+
+# Example
+print("ha" * 3)
+
+#Use Case - Style Your Logs
+#    |-----> use repeated characters to create clear sections in output
+style_log = "="*10
+print(style_log)
+print("Logs")
+print(style_log)
