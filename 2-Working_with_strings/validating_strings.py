51dc9bc2c978bd1f9c2867def1d0ab730ed39675
Alejandro Diaz <diaz360arteaga@gmail.com>
2026-04-02 20:21:14 -0600

    Final Code for validating_strings.py

---
+++ 2-Working_with_strings/validating_strings.py
@@ -0,0 +1,11 @@
+# Validation
+# isalpha() --> checks if the string has only letters
+# Check if the contry name contains only letters
+country = 'USA'
+print(country)
+print("The contry name contains only letters?",country.isalpha())
+
+# isnumeric() --> checks if the string has only numbers 
+phone = "0176122h3213"
+print(phone)
+print("The string has only numbers?",phone.isnumeric())
