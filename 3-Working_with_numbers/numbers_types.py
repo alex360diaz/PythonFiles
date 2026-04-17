2bebaaea2b530361e2899ee28a8e1c92da850a0a
Alejandro Diaz <diaz360arteaga@gmail.com>
2026-04-17 13:45:01 -0600

    Final Code Numbers types()

--- 3-Working_with_numbers/numbers_types.py
+++ 3-Working_with_numbers/numbers_types.py
@@ -1,9 +1,24 @@
 # Types of numbers
 x = 5
 y = 5.7
-z = 2 +3j
+z = 2 + 3j
 
 print(type(x))
 print(type(y))
 print(type(z))
 
+# Convert string to number --> we use int(name of the variable)
+
+num = "10"
+print(type(num))
+print(num)
+print(int(num) * 5)
+
+# Convert float to int --> we use int(name of the variable)
+
+num2 = 3.1416
+print(type(num2))
+print("Before convertion-->", num2)
+print("After convertion-->", int(num2))
+
+# We can convert anything int(), float(), str(), complex()
